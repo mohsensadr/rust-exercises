06836fae65ad3977676195c0bfc12cfc2bab9a36
Mohsen Sadr <mohsen.sadr91@gmail.com>
2025-08-18 22:48:30 +0200

    add bank -- in progress

---
+++ bank/src/main.rs
@@ -0,0 +1,41 @@
+#[derive(Debug)]
+struct Account{
+    id : u32,
+    holder : String,
+    balance: i32
+}
+
+impl Account{
+    fn new(id: u32, holder: String) -> Self{
+        Account{
+            id, 
+            holder,
+            balance: 0
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Bank{
+    accounts: Vec<Account>,
+}
+
+impl Bank{
+    fn new() -> Self{
+        Bank{accounts: vec![]}
+    }
+}
+
+fn print_account(account: Account){
+    println!("{:#?}", account);
+}
+
+fn main() {
+    let bank = Bank::new();
+    let account = Account::new(
+        1, String::from("me")
+    );
+
+    println!("{:#?}", bank);
+    print_account(account);
+}
