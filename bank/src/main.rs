bdc10a07bb466c100cc8aa7f151a310809d3f8f7
Mohsen Sadr <mohsen.sadr91@gmail.com>
2025-08-21 20:14:12 +0200

    complete Bank and Account Imp

--- bank/src/main.rs
+++ bank/src/main.rs
@@ -13,6 +13,20 @@ impl Account{
             balance: 0
         }
     }
+
+    fn summary(&self) -> String {
+        format!("{} has a balance of {}", self.holder, self.balance)
+    }
+    
+    fn deposit(&mut self, amount: i32) -> i32 {
+        self.balance += amount;
+        self.balance
+    } 
+
+    fn withdraw(&mut self, amount: i32) -> i32 { 
+        self.balance -= amount;
+        self.balance
+    }
 }
 
 #[derive(Debug)]
@@ -24,28 +38,32 @@ impl Bank{
     fn new() -> Self{
         Bank{accounts: vec![]}
     }
-}
 
-fn print_account(account: &Account){
-    println!("current account {:#?}", account);
-}
+    fn total_balance(&self) -> i32 {
+        self.accounts.iter().map(|account| account.balance).sum()
+    }
 
-fn add_account(bank: &mut Bank, account: Account){
-    bank.accounts.push(account);
+    fn summary(&self) -> Vec<String> { 
+        self.accounts.iter().map(|account| account.summary()).collect::<Vec<String>>()
+    }
+
+    fn add_account(&mut self, account: Account){
+        self.accounts.push(account);
+    }
 }
 
 fn main() {
     let mut bank = Bank::new();
-    let account = Account::new(
-        1, String::from("me")
-    );
+    let mut account = Account::new(1, String::from("Moos"));
+    let mut account2 = Account::new(2, String::from("John"));
 
-    println!("current bank status {:#?}", bank);
+    account.deposit(500);
+    account.withdraw(250);
+    account2.deposit(900);
 
-    // pass by reference
-    print_account(&account);
-    print_account(&account);
+    bank.add_account(account);
+    bank.add_account(account2);
 
-    add_account(&mut bank, account);
-    println!("current bank status {:#?}", bank);
+    println!("current bank summary: {:#?}", bank.summary());
+    println!("current bank balance: {}", bank.total_balance());
 }
