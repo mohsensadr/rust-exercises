ca32727524290dcbb2f5f8b00846f26b3e096392
Mohsen Sadr <mohsen.sadr91@gmail.com>
2025-08-25 19:56:50 +0200

    print erros of the file

--- logs/src/main.rs
+++ logs/src/main.rs
@@ -1,8 +1,25 @@
 use std::fs;
 
+fn extract_errors(text: &str) -> Vec<&str> {
+    let split_text = text.split("\n");
+
+    let mut results = vec![];
+
+    for line in split_text {
+        if line.starts_with("ERROR"){
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 fn main() {
     match fs::read_to_string("logs.txt") {
-        Ok(..) => println!("File read."),
+        Ok(text_that_was_read) => {
+            let error_logs = extract_errors(text_that_was_read.as_str());
+            println!("{:#?}", error_logs);
+        }
         Err(error) => println!("Error: {}", error)
     }
 }
